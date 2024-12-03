2aa769a2d272a8f88359c27166643cbf1739d9b1
bnewvillage <blainenewvillage@gmail.com>
2024-12-03 06:25:02 +0400

    initial

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,16 @@
+const currentYearElement =
+document.querySelector(`#currentYear`);
+
+const today = new Date();
+const currentYear = today.getFullYear();
+
+currentYearElement.innerHTML = `<span class="currentyear">${currentYear}</span>`
+
+//last modified code below
+const lastModifiedElement = 
+document.querySelector(`#lastModified`);
+
+const lastModified = today.getDate();
+
+lastModifiedElement.innerHTML = `<p class="lastModified">Last Modified: ${today}</p>`
+
